41cbe8bcfbd22bcc56c03e4d550332e9cdcd32c3
N2oBishrel <112277237+N2oBishrel@users.noreply.github.com>
2022-09-10 15:15:47 -0500

    Add files via upload

---
+++ Activity 2.py
@@ -0,0 +1,4 @@
+# This program converts an input Fahrenheit temperature to Celsius.
+celsius = float(input())
+fahrenheit = celsius * 9 / 5 + 32
+print(str(celsius) + "Celsius = " + str(fahrenheit) + "Fahrenhiet")
