a99d6d3c9e8a0fa9954816d9c605a2cc589bb3cb
Dave Braunschweig <dave@itrain.ms>
2022-09-13 09:24:00 -0500

    Folder structcure

--- Assignment 3/Activity 2.py
+++
@@ -1,5 +0,0 @@
-# This program prompts the user for the name of their dog and its age in human years.
-name = input()
-age = int(input())
-dogage = age * 7
-print(name + " is " + str(dogage) + " years old in dog years ")
--- Assignment 3/Activity 7.py
+++ Assignment 3/Activity 7.py
@@ -1,4 +1,5 @@
 # This program prompts the user for the name of their dog and its age in human years.
-max = float(input())
-dog = 4 * 7
-print(str(dog) + "Max = " + str(4) + "years")
+name = input()
+age = int(input())
+dogage = age * 7
+print(name + " is " + str(dogage) + " years old in dog years ")
