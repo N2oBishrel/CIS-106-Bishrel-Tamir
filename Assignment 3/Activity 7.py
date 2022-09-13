a7e152db7842f56292c3cd1c5db847699ed771bc
Dave Braunschweig <dave@itrain.ms>
2022-09-13 09:27:02 -0500

    Assignment 3 corrections

--- Assignment 3/Activity 7.py
+++ Assignment 3/Activity 7.py
@@ -1,5 +1,9 @@
 # This program prompts the user for the name of their dog and its age in human years.
+# References:
+# References go here ...
+print("Enter dog's name:")
 name = input()
+print("Enter dog's age in years:")
 age = int(input())
 dogage = age * 7
 print(name + " is " + str(dogage) + " years old in dog years ")
