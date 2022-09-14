3459241f71fc16710e4055af8c580651abd547f3
N2oBishrel <112277237+N2oBishrel@users.noreply.github.com>
2022-09-14 16:43:03 -0500

    Delete Assignment 4 directory

--- Assignment 4/Activity 3.py
+++
@@ -1,8 +0,0 @@
-hours = int(input("Enter hours worked per week:"))
-rate = int(input("Enter rate:"))
-grossPayWeek = hours*rate
-print(grossPayWeek)
-grossPayYear = grossPayWeek*52
-print(grossPayYear)
-grossPayMonth = grossPayYear/12
-print(grossPayMonth)
