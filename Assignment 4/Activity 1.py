c6d4ef8d1819bb911046cc3730ea46f65b0eb061
N2oBishrel <112277237+N2oBishrel@users.noreply.github.com>
2022-09-15 18:43:25 -0500

    Update Activity 1.py

--- Assignment 4/Activity 1.py
+++ Assignment 4/Activity 1.py
@@ -3,9 +3,9 @@ hours = int(input("Enter hours worked per week:"))
 rate = int(input("Enter rate:"))
 
 # Calculating weekly, yearly, monthly gross pay
-grossPayWeek = hours*rate
-grossPayYear = grossPayWeek*52
-grossPayMonth = grossPayYear/12
+grossPayWeek = hours * rate
+grossPayYear = grossPayWeek * 52
+grossPayMonth = grossPayYear / 12
 
 # Printing results
 print(grossPayYear)
