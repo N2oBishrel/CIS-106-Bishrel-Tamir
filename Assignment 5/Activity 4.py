fab999119f7c68b40a8ec158047806396c7bc119
N2oBishrel <112277237+N2oBishrel@users.noreply.github.com>
2022-09-30 16:46:16 -0500

    Add files via upload

---
+++ Assignment 5/Activity 4.py
@@ -0,0 +1,18 @@
+﻿def calculateCelsius(fahrenheit):
+    celsius = (fahrenheit - 32) * 5 / 9
+    
+    return celsius
+
+def displayResult(fahrenheit, celsius):
+    print(str(fahrenheit) + "°Fahrenheit is" + str(celsius) + "°Celsius")
+
+def getFahrenheit():
+    print("Enter Fahrenheit temperature:")
+    fahrenheit = float(input())
+    
+    return fahrenheit
+
+# Main
+fahrenheit = getFahrenheit()
+celsius = calculateCelsius(fahrenheit)
+displayResult(fahrenheit, celsius)
